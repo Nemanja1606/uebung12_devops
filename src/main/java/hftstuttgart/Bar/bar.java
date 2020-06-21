2782bda005cc7919310754df03b9dd3f14e34a24
Nemanja Vucetic <71vune1bwi@hft-stuttgart.de>
2020-06-21 16:16:22 +0200

    _

---
+++ src/main/java/hftstuttgart/Bar/bar.java
@@ -0,0 +1,14 @@
+package bar;
+
+import org.apache.log4j.Logger;
+
+
+public class bar {
+    
+    static Logger logger = Logger.getLogger(bar.class);
+
+    public void doIt() {
+      logger.debug("Did it again!");
+    }
+
+}
